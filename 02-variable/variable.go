f4d1b0af4f17d8662cf84283623d0891abfa9417
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2026-09-22 19:07:10 +0700

    variable

---
+++ 02-variable/variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// strings
+	var firstName string = "Syahrul"
+	var middleName = "Safarudin"
+	lastName := "Hasan"
+	fmt.Println(firstName, middleName, lastName)
+
+	// integers
+	var age int = 20
+	fmt.Println(age)
+
+	// boolean
+	var isMarried bool = false
+	fmt.Println(isMarried)
+
+	// float
+	var height float32 = 169.75
+	fmt.Println(height)
+}
